bfd3cd0426b488adc31cad58639ca75164925b83
archebaldo <zakharov90@gmail.com>
2024-07-28 21:51:32 +0300

    Update: webpack-config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,9 @@
 // libraries;
 import path from 'path';
+
+// plugins;
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import DotEnv from 'dotenv-webpack';
 
 // types;
 import { type Configuration } from 'webpack';
@@ -8,12 +11,13 @@ import { type Configuration as DevServerConfiguration } from 'webpack-dev-server
 
 // helpers;
 const Path = {
+  PUBLIC_PATH: `/`,
   ENTRY: `${path.resolve(__dirname, `src`, `index.tsx`)}`,
   DIST: `${path.resolve(__dirname, `dist`)}`,
   HTML: `${path.resolve(__dirname, `public`, `index.html`)}`,
 };
 
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 const createDevServer = (port: number): DevServerConfiguration => {
   return {
@@ -30,12 +34,14 @@ const config: Configuration = {
   output: {
     filename: `bundle.js`,
     path: Path.DIST,
+    publicPath: Path.PUBLIC_PATH,
   },
   devtool: `inline-source-map`,
   plugins: [
     new HtmlWebpackPlugin({
       template: Path.HTML,
     }),
+    new DotEnv(),
   ],
   module: {
     rules: [
