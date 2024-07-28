e0176b49771a73e3c83d061f13d4d67703fcc6ad
archebaldo <zakharov90@gmail.com>
2024-07-28 22:10:18 +0300

    Update: app

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,3 +1,44 @@
+// libraries;
+import { Routes, Route } from 'react-router-dom';
+
+// pages;
+import { Main } from '../../pages/main/main';
+import { Details } from '../../pages/details/details';
+import { NotFound } from '../../pages/not-found/not-found';
+
+// components;
+import { PageContainer } from '../page-container/page-container';
+
+// helpers;
+import { PagePath } from '../../helpers/const';
+
 export const App = () => {
-  return <div>Hello world!</div>;
+  return (
+    <Routes>
+      <Route
+        path={PagePath.MAIN}
+        element={
+          <PageContainer isMainPage={true}>
+            <Main />
+          </PageContainer>
+        }
+      />
+      <Route
+        path={PagePath.DETAILS}
+        element={
+          <PageContainer>
+            <Details />
+          </PageContainer>
+        }
+      />
+      <Route
+        path={PagePath.NOT_FOUND}
+        element={
+          <PageContainer>
+            <NotFound />
+          </PageContainer>
+        }
+      />
+    </Routes>
+  );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,22 @@
 // libraries;
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 // components;
 import { App } from './components/app/app';
+
+// store;
+import { store } from './store/store';
 
 // styles;
 import './style.scss';
 
 const root = createRoot(document.getElementById(`root`) as HTMLDivElement);
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
